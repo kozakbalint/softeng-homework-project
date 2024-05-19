1110bf3de4a917995390a4b84bc56d3d284cee17
Bálint Kozák <kozak.balint15@gmail.com>
2024-05-19 10:54:04 +0200

    test: testing the cli input parser

--- src/main/java/foxgame/cli/Main.java
+++ src/main/java/foxgame/cli/Main.java
@@ -10,21 +10,19 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
         FoxGameState gameState = new FoxGameState();
-        var parser = new Function<String, Position>() {
-            @Override
-            public Position apply(String s) {
-                s = s.trim();
-                if (!s.matches("\\d+\\s+\\d+")) {
-                    throw new IllegalArgumentException();
-                }
-                var scanner = new Scanner(s);
-                return new Position(scanner.nextInt(), scanner.nextInt());
-            }
-        };
 
-        TwoPhaseMoveGame<Position> cli = new TwoPhaseMoveGame<>(gameState, parser);
+        TwoPhaseMoveGame<Position> cli = new TwoPhaseMoveGame<>(gameState, Main::parsePosition);
         System.out.println("Usage: <row> <col>");
         System.out.println("Example: 0 2");
         cli.start();
+    }
+
+    public static Position parsePosition(String s) {
+        s = s.trim();
+        if (!s.matches("\\d+\\s+\\d+")) {
+            throw new IllegalArgumentException();
+        }
+        var scanner = new Scanner(s);
+        return new Position(scanner.nextInt(), scanner.nextInt());
     }
 }
