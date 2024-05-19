c329907d71c6042a478ff4c787480ed7724f063f
Bálint Kozák <kozak.balint15@gmail.com>
2024-05-19 17:39:49 +0200

    feat: new game menu

---
+++ src/main/java/foxgame/gui/NewGameController.java
@@ -0,0 +1,44 @@
+package foxgame.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+
+import java.io.IOException;
+
+public class NewGameController {
+    @FXML
+    private Button newgameButton;
+
+    @FXML
+    private TextField playerOneNameField;
+
+    @FXML
+    private TextField playerTwoNameField;
+
+    @FXML
+    private void initialize() {
+        playerOneNameField.textProperty().addListener((observable, oldValue, newValue) -> playerTwoNameField.setDisable(newValue.isEmpty()));
+
+        playerTwoNameField.textProperty().addListener((observable, oldValue, newValue) -> newgameButton.setDisable(newValue.isEmpty()));
+    }
+
+    public void onNewGame(ActionEvent event) throws IOException {
+        Logger.debug("Starting new game with players {} and {}", playerOneNameField.getText(), playerTwoNameField.getText());
+        FoxGameController controller = new FoxGameController();
+        controller.setPlayerNames(playerOneNameField.getText(), playerTwoNameField.getText());
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/game.fxml"));
+        fxmlLoader.setController(controller);
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
